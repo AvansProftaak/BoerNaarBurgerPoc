87b73c91835c7a9193b288d24c14ca7a3fad9027
zafuso <71095771+zafuso@users.noreply.github.com>
2021-05-06 14:42:05 +0200

    add comments to framework code

--- app/Framework/Controller.php
+++ app/Framework/Controller.php
@@ -1,17 +1,19 @@
 <?php
 
 class Controller {
+    // require the model
     public function model($model) {
         require_once '../app/models/' . $model . '.php';
+        // instantiate the model
         return new $model();
     }
 
-
+    // require the view, if exists.
     public function view($view, $data = []) {
         if (file_exists('../app/views/' . $view . '.php')) {
             require_once '../app/views/' . $view . '.php';
         } else {
-            die("View does not exists.");
+            die("View does not exist.");
         }
     }
 }
--- app/Framework/Core.php
+++ app/Framework/Core.php
@@ -1,6 +1,8 @@
 <?php
 
 class Core {
+    // default is /pages/index. If invalid input is provided user is redirected to homepage
+    // TODO: create 404 page and set as default
     protected $currentController = 'Pages';
     protected string $currentMethod = 'index';
     protected array $params = [];
@@ -8,13 +10,16 @@ class Core {
     public function __construct() {
         $url = $this->getUrl();
 
+        // open controller based on first value of url array
         if (file_exists('../app/Controllers/' . ucwords($url[0]) . '.php')) {
             $this->currentController = ucwords($url[0]);
             unset($url[0]);
         }
+        // require the controller
         require_once '../app/Controllers/' . $this->currentController . '.php';
         $this->currentController = new $this->currentController;
 
+        // in this controller call the method based on second url value
         if (isset($url[1])) {
             if (method_exists($this->currentController, $url[1])) {
                 $this->currentMethod = $url[1];
@@ -22,10 +27,14 @@ class Core {
             }
         }
 
+        // get parameters
         $this->params = $url ? array_values($url) : [];
+
+        //call the function in the controller, with array of params (if any)
         call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
+    // splits the url at '/' into an array of each parameter, input is trimmed and sanitized.
     public function getUrl() {
         if(isset($_GET['url'])) {
             $url = rtrim($_GET['url'], '/');
