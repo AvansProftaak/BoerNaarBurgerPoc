b92ea22af56f728013f463310884eaa8a0b5915d
Shabular <itsasecret@notthis.com>
2021-05-24 16:18:27 +0200

    removed :8080 from config pshed by mac-user

--- app/Config/Config.php
+++ app/Config/Config.php
@@ -7,5 +7,5 @@ define('DB_NAME', 'boer_naar_burger');
 
 define('APPROOT', dirname(dirname(__FILE__)));
 define('IMGROOT', dirname(dirname(dirname(__FILE__))) . '\public\img');
-define('URLROOT', 'http://localhost:8080/BoerNaarBurgerPoc');
+define('URLROOT', 'http://localhost/BoerNaarBurgerPoc');
 define('SITENAME', 'Boer naar Burger');
