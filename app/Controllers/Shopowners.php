8bdf80e6722f76fe3f570e196b68fc712a9549f1
Shabular <itsasecret@notthis.com>
2021-06-03 12:05:13 +0200

    removed errors bij changing from $this->view to header

--- app/Controllers/Shopowners.php
+++ app/Controllers/Shopowners.php
@@ -18,7 +18,7 @@ class Shopowners extends Controller
     public function create() {
 
         if (isLoggedIn()){
-            $this->view('pages/index');
+            header('location: ' . URLROOT . '/pages/index');
         }
 
         $data = [
@@ -154,7 +154,7 @@ class Shopowners extends Controller
     public function register() {
 
         if (isLoggedIn()){
-            $this->view('pages/index');
+            header('location: ' . URLROOT . '/pages/index');
         }
 
         $data = [
@@ -283,7 +283,7 @@ class Shopowners extends Controller
     public function login() {
 
         if (isLoggedIn()){
-            $this->view('pages/index');
+            header('location: ' . URLROOT . '/pages/index');
         }
 
         $data = [
@@ -356,7 +356,7 @@ class Shopowners extends Controller
 
     public function updateitems() {
         if (isLoggedIn()){
-            $this->view('pages/index');
+            header('location: ' . URLROOT . '/pages/index');
         }
         $data = [
             'item_name'            => '',
@@ -443,7 +443,7 @@ class Shopowners extends Controller
 
     public function myShop() {
         if (isLoggedIn()){
-            $this->view('pages/index');
+            header('location: ' . URLROOT . '/pages/index');
         }
         $data = $this->shopOwnerModel->getMyShop();
         $this->view('shopowners/myShop', $data);
@@ -602,7 +602,7 @@ class Shopowners extends Controller
     public function changePassword()
     {
         if (isLoggedIn()){
-            $this->view('pages/index');
+            header('location: ' . URLROOT . '/pages/index');
         }
         
         if (isLoggedInShopOwner()) {
