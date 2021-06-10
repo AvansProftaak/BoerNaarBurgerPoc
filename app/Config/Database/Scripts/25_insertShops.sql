329a8dc93050c8122cc57bfd6a7c7f8b7d335ddc
zafuso <71095771+zafuso@users.noreply.github.com>
2021-06-10 14:48:35 +0200

    add some shops and update sql scrripts

--- app/Config/Database/Scripts/25_insertShops.sql
+++ app/Config/Database/Scripts/25_insertShops.sql
@@ -1,4 +1,6 @@
 INSERT INTO boer_naar_burger.shops (kvk_number, shop_name, description, address, house_number, postal_code, city, country, banner_url)
 VALUES ('37564967', '4adb920c-6a32-4f11-a6ef-c6d9f30a7749', 'bc3d282f-cee9-4e16-ae53-5e5d54ff3e94', 'Veestraat', '12', '5748KF', 'Winschoten', 'NL', '/assets/shopbanners/6yLuyVdOG4.png'),
 ('06989770', '368a83c6-af51-491d-8f7b-bda06dae20c7', 'ba04b20f-bde4-497a-aa6b-bdd3c15eff57', 'Spoorlaan', '7A', '4813BK', 'Breda', 'NL', '/assets/shopbanners/e8eEmiwsjS.png'),
-('27798339', 'c58921c5-7073-41b8-a4b1-0aabc61e4e7e', '3fae8080-e9b2-4706-b730-e762c2da8fbd', 'Bergpad', '67', '2049TJ', 'Tilburg', 'NL', '/assets/shopbanners/LmY1ETcsyE.png');
+('27798339', 'c58921c5-7073-41b8-a4b1-0aabc61e4e7e', '3fae8080-e9b2-4706-b730-e762c2da8fbd', 'Bergpad', '67', '2049TJ', 'Tilburg', 'NL', '/assets/shopbanners/LmY1ETcsyE.png'),
+('56787654', 'ac9170b5-edea-4719-9f54-9d7c4fa7d349', '82a61de5-8507-411c-8205-289bbabeb253', 'Westkapelseweg', '10', '4374BB', 'Zoutelande', 'NL', '/assets/shopbanners/f7eTgjyi7S.png'),
+('78697033', '4aa6c8fd-d06e-4b1c-bb53-f524762f4621', '3180ddff-a36c-47c2-9676-f502c8b51d72', 'Salvador Allendelaan', '9', '5231BZ', 'Den Bosch', 'NL', '/assets/shopbanners/qvQWLbyee6.png');
